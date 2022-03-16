6285c8c2c217f5ca4586b697b0ba3f19260bda37
John Napier <jcee.napier@gmail.com>
2022-03-16 10:24:36 -0600

    Add circle ci

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -1,6 +1,9 @@
-class TmdbService
+
+
+dclass TmdbService
   def self.conn
     Faraday.new(url: 'https://api.themoviedb.org') do |f|
+      require 'pry'; binding.pry
       f.params['api_key'] = ENV['tmdb_key']
     end
   end
